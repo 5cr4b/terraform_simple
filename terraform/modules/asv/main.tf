33f55f627d1f5e0c2816a887df8836a36c7b420a
Nam Van Bui <namvan.bui@dxc.com>
2022-01-08 18:47:09 +0700

    modify use32 bit

--- terraform/modules/asv/main.tf
+++ terraform/modules/asv/main.tf
@@ -4,11 +4,10 @@ resource "azurerm_app_service" "name" {
   resource_group_name = var.config.resource_group_name
   app_service_plan_id = var.config.app_service_plan_id
   https_only = var.config.https_only
-  use_32_bit_worker_process = var.config.use_32_bit_worker_process
+  
   site_config {
       linux_fx_version = var.config.linux_fx_version 
+      use_32_bit_worker_process = var.config.use_32_bit_worker_process
   }
   app_settings = var.config.app_settings
-   
-
 }
